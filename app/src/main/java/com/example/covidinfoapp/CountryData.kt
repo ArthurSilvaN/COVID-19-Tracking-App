41b9f09fbfb30b509a86c16ef1b8501d25dbb676
arthur.nunes <arthur.nunes@dtidigital.com.br>
2021-04-29 16:46:35 -0300

    add data class Country

---
+++ app/src/main/java/com/example/covidinfoapp/CountryData.kt
@@ -0,0 +1,12 @@
+package com.example.covidinfoapp
+
+import com.google.gson.annotations.SerializedName
+
+data class CountryData(
+    @SerializedName("updated") val update: Long,
+    @SerializedName("recovered") val recovered: Int,
+    @SerializedName("active") val active: Int,
+    @SerializedName("deaths") val deaths: Int,
+    @SerializedName("cases") val cases: Int,
+    @SerializedName("country") val countries: String
+)
